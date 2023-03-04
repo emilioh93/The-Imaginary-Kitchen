a4f790c2561f1e1e0b7bb1f14de5542248793742
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2023-03-04 19:00:49 -0300

    Added models

---
+++ backend/src/models/Menu.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const menuSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+});
+
+const Menu = mongoose.model('Menu', menuSchema);
+
+export default Menu;
