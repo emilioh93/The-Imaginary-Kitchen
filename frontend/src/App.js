ea803c3bd8b5d3c003bf619b69fb46ab251a9c77
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2023-03-05 19:30:44 -0300

    Added menu page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './common/components/Navbar';
 import HomePage from './pages/Home/HomePage';
-import { PageTwo } from './pages/Page2';
+import Menu from './pages/Menu/Menu';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Switch>
       <Route path='/' exact component={HomePage} />
-      <Route path='/page2' exact component={PageTwo} />
+      <Route path='/menus' exact component={Menu} />
     </Switch>
   </BrowserRouter>
 );
