b4993dc436c02d5fbbc58317617db845dcdaad60
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2023-03-07 21:44:16 -0300

    Added add new button

--- frontend/src/common/components/AdminTable.js
+++ frontend/src/common/components/AdminTable.js
@@ -1,52 +1,63 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { IconButton } from '@mui/material';
 import EditSharpIcon from '@mui/icons-material/EditSharp';
 import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+} from '@mui/material';
 
 export default function AdminTable({ rows, columns }) {
   console.log('🚀 ~ rows:', rows);
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-        <TableHead>
-          <TableRow>
-            {columns.map(column => (
-              <TableCell key={column.id}>{column.label}</TableCell>
-            ))}
-            <TableCell>Edit</TableCell>
-            <TableCell>Delete</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              {row.atributtes.map((item, index) => (
-                <TableCell key={index}>{item}</TableCell>
+    <>
+      <Box display='flex' justifyContent='flex-end' marginBottom='1rem'>
+        <Button variant='contained' color='success'>
+          Add new
+        </Button>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell key={column.id}>{column.label}</TableCell>
               ))}
-              <TableCell>
-                <IconButton aria-label='edit' color='info'>
-                  <EditSharpIcon />
-                </IconButton>
-              </TableCell>
-              <TableCell>
-                <IconButton aria-label='delete' color='error'>
-                  <DeleteSharpIcon />
-                </IconButton>
-              </TableCell>
+              <TableCell>Edit</TableCell>
+              <TableCell>Delete</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                {row.atributtes.map((item, index) => (
+                  <TableCell key={index}>{item}</TableCell>
+                ))}
+                <TableCell>
+                  <IconButton aria-label='edit' color='info'>
+                    <EditSharpIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell>
+                  <IconButton aria-label='delete' color='error'>
+                    <DeleteSharpIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
